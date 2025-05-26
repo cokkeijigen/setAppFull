82c9132b9b497b92433975eb7ee31f05cb096e1f
cokkeijigen <itsukezigen@gmail.com>
2025-05-27 00:12:59 +0800

    Update SettingsActivity.java

--- SetAppFull/app/src/main/java/ss/colytitse/setappfull/app/SettingsActivity.java
+++ SetAppFull/app/src/main/java/ss/colytitse/setappfull/app/SettingsActivity.java
@@ -1,10 +1,5 @@
 package ss.colytitse.setappfull.app;
 
-import static ss.colytitse.setappfull.app.AppSettings.getHelloWorld;
-import static ss.colytitse.setappfull.app.AppSettings.getScopeMode;
-import static ss.colytitse.setappfull.app.AppSettings.setActivityStatusBar;
-import static ss.colytitse.setappfull.app.AppSettings.setHelloWorld;
-import static ss.colytitse.setappfull.app.AppSettings.setScopeMode;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
@@ -13,6 +8,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -20,33 +16,35 @@ import androidx.annotation.Nullable;
 
 import ss.colytitse.setappfull.R;
 
-public class SettingsActivity extends Activity {
-
-    private Context mContext;
+public class SettingsActivity extends Activity implements CompoundButton.OnCheckedChangeListener {
 
     @Override @SuppressLint("UseSwitchCompatOrMaterialCode")
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setActivityStatusBar(this);
-        this. mContext = getApplicationContext();
-        setContentView(R.layout.settings_layout);
-        Switch scope_mode_switch = findViewById(R.id.scope_mode_switch);
-        scope_mode_switch.setChecked(getScopeMode(mContext));
-        scope_mode_switch.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            if (isChecked != getScopeMode(mContext))
-                setScopeMode(mContext, isChecked);
-        });
-        if(getHelloWorld(mContext)){
+        AppSettings.setActivityStatusBar(this);
+        this.setContentView(R.layout.settings_layout);
+        Switch scopeModeSwitch = findViewById(R.id.scope_mode_switch);
+        scopeModeSwitch.setChecked(AppSettings.getScopeMode(this));
+        scopeModeSwitch.setOnCheckedChangeListener(this);
+        if(AppSettings.getHelloWorld(this)){
             TextView HelloWorld = findViewById(R.id.hello_world_view);
-            HelloWorld.setText(String.format("%s\n%s", HelloWorld.getText(), getResources().getString(R.string.hello_world_1)));
-            setHelloWorld(mContext);
-            setScopeMode(mContext, true);
+            HelloWorld.setText(String.format("%s\n%s", HelloWorld.getText(),
+                    getResources().getString(R.string.hello_world_1)));
+            AppSettings.setHelloWorld(this);
+            AppSettings.setScopeMode(this, true);
         }
     }
 
     public void openGitHub(View view) {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("https://github.com/cokkeijigen/setAppFull"));
-        startActivity(intent);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (isChecked != AppSettings.getScopeMode(this)) {
+            AppSettings.setScopeMode(this, isChecked);
+        }
     }
 }
