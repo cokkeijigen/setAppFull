049b86e6be2aeb7ae3dab8677e8df212bdd18a35
cokkeijigen <itsukezigen@gmail.com>
2025-05-27 00:13:05 +0800

    Update MainHook.java

--- SetAppFull/app/src/main/java/ss/colytitse/setappfull/MainHook.java
+++ SetAppFull/app/src/main/java/ss/colytitse/setappfull/MainHook.java
@@ -1,41 +1,49 @@
 package ss.colytitse.setappfull;
 
-import static android.view.WindowManager.LayoutParams.*;
-import static de.robv.android.xposed.XposedHelpers.*;
+import android.view.WindowManager.LayoutParams;
+import de.robv.android.xposed.XposedHelpers;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
 import androidx.annotation.RequiresApi;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.util.Date;
-
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XSharedPreferences;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
+
+
 @SuppressWarnings("unused")
 public class MainHook implements IXposedHookLoadPackage {
 
-    private static final String TAG = "debug_test_";
-    private String SystemModeList = "";
-    private String AppModeList = "";
+    private static final String TAG = "MainHook";
+    private List<String> SystemModeList = List.of();
+    private List<String> AppModeList = List.of();
+    private int statusBarHeightId = 0;
     private boolean IsScopeMode = true;
     private long lastUpdateTime = 0;
 
-    private void update()
-    {
+    private void update() {
         XSharedPreferences xsp = new XSharedPreferences(BuildConfig.APPLICATION_ID,"config");
         xsp.makeWorldReadable();
-        SystemModeList = xsp.getString("SystemMode", "");
-        AppModeList = xsp.getString("TimelyMode", "");
-        IsScopeMode = xsp.getBoolean("scope_mode_switch", true);
-        lastUpdateTime = System.currentTimeMillis();
+        String SystemModeString = xsp.getString("SystemMode", "");
+        this.SystemModeList = Arrays.stream(SystemModeString.split("#"))
+                .filter(e-> !e.isEmpty())
+                .collect(Collectors.toList());
+        String AppModeString = xsp.getString("TimelyMode", "");
+        this.AppModeList = Arrays.stream(AppModeString.split("#"))
+                .filter(e-> !e.isEmpty())
+                .collect(Collectors.toList());
+        this.IsScopeMode = xsp.getBoolean("scope_mode_switch", true);
+        this.lastUpdateTime = System.currentTimeMillis();
     }
 
     @Override
@@ -49,22 +57,21 @@ public class MainHook implements IXposedHookLoadPackage {
                 update();
                 onSystemMode(lpparam);
             } catch (Throwable ignored){}
-        } else if(IsScopeMode || AppModeList.contains(lpparam.packageName)) {
+        } else if(this.IsScopeMode || this.AppModeList.contains(lpparam.packageName)) {
             try{
-                onAppMode();
+                onAppMode(lpparam);
             } catch (Throwable ignored){}
         }
     }
 
     private void onSystemMode(XC_LoadPackage.LoadPackageParam lpparam) {
-        findAndHookMethod("com.android.server.wm.DisplayPolicy", lpparam.classLoader,
-                "layoutWindowLw", "com.android.server.wm.WindowState",
-                "com.android.server.wm.WindowState", "com.android.server.wm.DisplayFrames",
+        XposedHelpers.findAndHookMethod("com.android.server.wm.DisplayPolicy", lpparam.classLoader, "layoutWindowLw",
+                "com.android.server.wm.WindowState", "com.android.server.wm.WindowState", "com.android.server.wm.DisplayFrames",
                 new XC_MethodHook() {
                     @Override
                     @RequiresApi(api = Build.VERSION_CODES.P)
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        WindowManager.LayoutParams attrs = (WindowManager.LayoutParams) getObjectField(param.args[0], "mAttrs");
+                        WindowManager.LayoutParams attrs = (WindowManager.LayoutParams) XposedHelpers.getObjectField(param.args[0], "mAttrs");
                         if (attrs.type > WindowManager.LayoutParams.LAST_APPLICATION_WINDOW)
                             return;
                         if(attrs.packageName.equals(BuildConfig.APPLICATION_ID)) {
@@ -73,40 +80,100 @@ public class MainHook implements IXposedHookLoadPackage {
                             }
                         }
                         else if (SystemModeList.contains(attrs.packageName)) {
-                            attrs.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+                            attrs.layoutInDisplayCutoutMode = LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
                         }
                     }
                 }
         );
-
     }
 
-    private void onAppMode() {
+    private void onAppMode(XC_LoadPackage.LoadPackageParam lpparam) {
+        try {
+            XposedHelpers.findAndHookMethod("android.view.View", lpparam.classLoader, "setSystemUiVisibility", int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) {
+                    param.args[0] = View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                }
+            });
+        } catch (Throwable ignored){}
 
-        findAndHookMethod(Activity.class, "onCreate", Bundle.class, new XC_MethodHook() {
-            @Override @SuppressLint("NewApi")
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-                Activity activity = (Activity) param.thisObject;
-                activity.getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                );
-                WindowManager.LayoutParams lp = activity.getWindow().getAttributes();
-                lp.layoutInDisplayCutoutMode = LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
-                activity.getWindow().setAttributes(lp);
-            }
-        });
+        try {
+            XposedHelpers.findAndHookMethod("android.view.Window", lpparam.classLoader, "setFlags", int.class, int.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    param.args[0] = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+                    param.args[1] = WindowManager.LayoutParams.FLAG_FULLSCREEN;
+                }
+            });
+        } catch (Throwable ignored) { }
 
-        findAndHookMethod(WindowInsetsCompat.class, "getDisplayCutout", new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                param.setResult(null);
-            }
-        });
+        try {
+            XposedHelpers.findAndHookMethod("android.view.Window", lpparam.classLoader, "setAttributes",
+                    "android.view.WindowManager.LayoutParams",
+                    new XC_MethodHook() {
+                        @Override
+                        @RequiresApi(api = Build.VERSION_CODES.P)
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            WindowManager.LayoutParams lp = (WindowManager.LayoutParams)param.args[0];
+                            lp.layoutInDisplayCutoutMode = LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+                        }
+                    }
+            );
+        } catch (Throwable ignored) { }
+
+        try {
+            XposedHelpers.findAndHookMethod("android.app.Activity", lpparam.classLoader, "onCreate", Bundle.class,
+                    new XC_MethodHook() {
+                        @Override
+                        @RequiresApi(api = Build.VERSION_CODES.P)
+                        @SuppressLint({"DiscouragedApi", "InternalInsetResource"})
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            Activity activity = (Activity) param.thisObject;
+                            if(statusBarHeightId == 0) {
+                                statusBarHeightId = activity.getResources().getIdentifier("status_bar_height", "dimen", "android");
+                            }
+
+                            Window window = activity.getWindow();
+                            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+                            WindowManager.LayoutParams lp = window.getAttributes();
+                            lp.layoutInDisplayCutoutMode = LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+                            activity.getWindow().setAttributes(lp);
+                        }
+                    }
+            );
+        } catch (Throwable ignored){}
+
+        try {
+            XposedHelpers.findAndHookMethod("android.content.res.Resources", lpparam.classLoader,"getDimensionPixelSize", int.class,
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            int resourceId = (int)param.args[0];
+                            if(statusBarHeightId != 0 && resourceId == statusBarHeightId ) {
+                                param.setResult(0);
+                            }
+                        }
+                    });
+        } catch (Throwable ignored) { }
+
+        try {
+            XposedHelpers.findAndHookMethod("androidx.core.view.WindowInsetsCompat", lpparam.classLoader, "getDisplayCutout",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            param.setResult(null);
+                    }
+            });
+        } catch (Throwable ignored) { }
     }
-
 }
