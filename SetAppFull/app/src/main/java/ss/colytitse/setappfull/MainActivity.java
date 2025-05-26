a5ef42c958c430994f5274a97e045896bfc6086d
cokkeijigen <itsukezigen@gmail.com>
2025-05-27 00:13:03 +0800

    Update MainActivity.java

--- SetAppFull/app/src/main/java/ss/colytitse/setappfull/MainActivity.java
+++ SetAppFull/app/src/main/java/ss/colytitse/setappfull/MainActivity.java
@@ -1,206 +1,198 @@
 package ss.colytitse.setappfull;
 
-import static ss.colytitse.setappfull.app.AppSettings.*;
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.Nullable;
-
-import java.util.ArrayList;
-import java.util.List;
-
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import ss.colytitse.setappfull.app.AppInfoAdapter;
+import ss.colytitse.setappfull.app.AppInfoManager;
+import ss.colytitse.setappfull.app.AppSettings;
 import ss.colytitse.setappfull.app.SettingsActivity;
 
+@SuppressWarnings({"unused"})
 @SuppressLint({"UseSwitchCompatOrMaterialCode","UseCompatLoadingForDrawables","SetTextI18n"})
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, SwipeRefreshLayout.OnRefreshListener, android.text.TextWatcher, TextView.OnEditorActionListener {
 
-    private static final String TAG = "test_";
-    private List<PackageInfo> systemAppList;
-    private List<PackageInfo> userAppList;
-    private boolean EditSearchInit = false;
-    private Context mContext;
-
-    private void initApplicationList() {
-        List<PackageInfo> allAppList = getPackageManager().getInstalledPackages(0);
-        systemAppList = new ArrayList<>();
-        userAppList = new ArrayList<>();
-        for (PackageInfo app : allAppList){
-            if ((app.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
-                userAppList.add(app);
-            else systemAppList.add(app);
-        }
-    }
+    private static final String TAG = "MainActivity";
+    private AppInfoManager  mAppInfoManager;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private ListView mAppListView;
+    private EditText mSearchEditText;
+    private TextView mListTypeName;
+    private ImageButton mSearchButton;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setActivityStatusBar(this);
-        setContentView(R.layout.main_layout);
-        mContext = getApplicationContext();
-        initApplicationList();
-        initMainActivity();
+        AppSettings.setActivityStatusBar(this);
+        this.setContentView(R.layout.main_layout);
+        TextView versionName = findViewById(R.id.version_name);
+        versionName.setText(String.format("%s%s",
+                versionName.getText().toString().split("->")[0],
+                BuildConfig.VERSION_NAME)
+        );
+
+        this.mSwipeRefreshLayout = this.findViewById(R.id.swipe_refresh_layout);
+        this.mSwipeRefreshLayout.setOnRefreshListener(this);
+        this.mAppListView = this.findViewById(R.id.app_items);
+        this.mAppListView.setOnItemClickListener(this);
+        this.mSearchEditText = this.findViewById(R.id.edit_insearch);
+        this.mSearchEditText.addTextChangedListener(this);
+        this.mSearchEditText.setOnEditorActionListener(this);
+        this.mListTypeName = this.findViewById(R.id.app_list_name);
+        this.mSearchButton = this.findViewById(R.id.btn_insearch);
+        int switchListType = AppSettings.getSwitchListType(this);
+        int stringId = switchListType == AppSettings.USER_VIEW ?
+                R.string.list_user : R.string.list_system;
+        this.mListTypeName.setText(String.format("(%s)",
+                this.getResources().getString(stringId))
+        );
+
+        this.mAppInfoManager = new AppInfoManager(this);
+        this.updateAppListView();
     }
 
     @Override
-    public void onAttachedToWindow() {
-        super.onAttachedToWindow();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (getHelloWorld(mContext)) {
-            View decorView = this.getWindow().getDecorView();
-            decorView.post(() -> {
-                Intent intent = new Intent();
-                intent.setClass(mContext, SettingsActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                mContext.startActivity(intent);
-            });
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        TextView pkgNameView = view.findViewById(R.id.app_pkgn);
+        String pkgName = pkgNameView.getText().toString();
+        LinearLayout itemBac = view.findViewById(R.id.item_root_view);
+        TextView modeText = view.findViewById(R.id.mode_state);
+        Switch itemSwitch = view.findViewById(R.id.set_switch);
+        boolean scopeMode = AppSettings.getScopeMode(this);
+        if(!itemSwitch.isChecked() && !scopeMode) {
+            itemSwitch.setChecked(true);
+            itemBac.setBackground(this.getResources().getDrawable(R.drawable.button_background2, this.getTheme()));
+            modeText.setText(R.string.mode_1);
+            AppSettings.saveAppMode(this, pkgName);
         }
+        else if (AppSettings.getSetMode(this, pkgName) == AppSettings.MODE_1 || (scopeMode && !itemSwitch.isChecked())){
+            itemSwitch.setChecked(true);
+            AppSettings.saveSystemMode(this, pkgName);
+            modeText.setText(R.string.mode_2);
+            itemBac.setBackground(this.getResources().getDrawable(R.drawable.button_background3, this.getTheme()));
+        }
+        else {
+            itemSwitch.setChecked(false);
+            modeText.setText("");
+            itemBac.setBackground(this.getResources().getDrawable(R.drawable.button_background, this.getTheme()));
+            AppSettings.deleteSelection(this, pkgName);
+        }
+        modeText.setVisibility(scopeMode ? View.GONE : View.VISIBLE);
     }
 
-    private void initMainActivity() {
-        int AppViewList = getOnSwitchListView(mContext);
-        TextView setVersionName = findViewById(R.id.version_name);
-        TextView appListName = findViewById(R.id.app_list_name);
-        setVersionName.setText(String.format("%s%s",
-                setVersionName.getText().toString().split("->")[0], getVersionName(mContext)));
-        appListName.setText(String.format("(%s)",getResources().getString(
-                AppViewList == USER_VIEW ? R.string.list_user : R.string.list_system)));
-        initMainActivityListView(AppViewList == USER_VIEW ? userAppList : systemAppList);
+    private void updateAppListView(String search) {
+        this.mSwipeRefreshLayout.setRefreshing(true);
+        String content = search != null ? search :
+                this.mSearchEditText.getText().toString();
+        int switchListType = AppSettings.getSwitchListType(this);
+        if(switchListType == AppSettings.USER_VIEW) {
+            this.mAppListView.setAdapter(new AppInfoAdapter(content.isEmpty()?
+                    this.mAppInfoManager.getUserAppList() :
+                    this.mAppInfoManager.userAppListFilter(content),
+                    this)
+            );
+        }
+        else {
+            this.mAppListView.setAdapter(new AppInfoAdapter(content.isEmpty()?
+                    this.mAppInfoManager.getSystemAppList() :
+                    this.mAppInfoManager.systemAppListFilter(content),
+                    this)
+            );
+        }
+        this.mSwipeRefreshLayout.setRefreshing(false);
     }
 
-    public void initMainActivityListView(List<PackageInfo> appList){
-        ListView listView = findViewById(R.id.app_items);
-        listView.setAdapter(new AppInfoAdapter(appList, mContext));
-        listView.setOnItemClickListener((adapterView, view, i, l)
-        -> {
-            TextView app_pkgn = view.findViewById(R.id.app_pkgn);
-            String pkgn = app_pkgn.getText().toString();
-            LinearLayout item_bac = view.findViewById(R.id.item_root_view);
-            TextView mode_text = view.findViewById(R.id.mode_state);
-            Switch onSwitch = view.findViewById(R.id.set_switch);
-            boolean scopeMode = getScopeMode(mContext);
-            if(!onSwitch.isChecked() && !scopeMode){
-                onSwitch.setChecked(true);
-                item_bac.setBackground(mContext.getResources().getDrawable(R.drawable.button_background2, mContext.getTheme()));
-                mode_text.setText(R.string.mode_1);
-                saveAppMode(mContext, pkgn);
-//                Toast.makeText(mContext,
-//                        getResources().getString(R.string.show_message_1),
-//                        Toast.LENGTH_SHORT
-//                ).show();
-            } else if (getSetMode(mContext, pkgn) == MODE_1 || (scopeMode && !onSwitch.isChecked())){
-                onSwitch.setChecked(true);
-                saveSystemMode(mContext, pkgn);
-                mode_text.setText(R.string.mode_2);
-                item_bac.setBackground(mContext.getResources().getDrawable(R.drawable.button_background3, mContext.getTheme()));
-//                Toast.makeText(mContext,
-//                        getResources().getString(R.string.show_message),
-//                        Toast.LENGTH_SHORT
-//                ).show();
-            }else {
-                onSwitch.setChecked(false);
-                mode_text.setText("");
-                view.findViewById(R.id.item_root_view).setBackground(
-                        mContext.getResources().getDrawable(R.drawable.button_background, mContext.getTheme())
-                );
-                deleteSelection(mContext, pkgn);
-            }
-            mode_text.setVisibility(scopeMode ? View.GONE : View.VISIBLE);
-        });
+    private void updateAppListView() {
+        this.updateAppListView(null);
     }
 
     public void onSwitchListView(View view) {
-        EditText edit_insearch = findViewById(R.id.edit_insearch);
-        String inText = edit_insearch.getText().toString();
-        TextView appListName = findViewById(R.id.app_list_name);
-        if(getOnSwitchListView(mContext) == USER_VIEW){
-            savonSwitch(mContext ,SYSTEM_VIEW);
-            initMainActivityListView(searchAppView(inText));
-            appListName.setText(String.format("(%s)", getResources().getString(R.string.list_system)));
+        if(AppSettings.getSwitchListType(this) == AppSettings.USER_VIEW) {
+            AppSettings.savonSwitch(this ,AppSettings.SYSTEM_VIEW);
+            this.updateAppListView();
+            this.mListTypeName.setText(String.format("(%s)",
+                    this.getResources().getString(R.string.list_system))
+            );
         }else {
-            savonSwitch(mContext, USER_VIEW);
-            initMainActivityListView(searchAppView(inText));
-            appListName.setText(String.format("(%s)", getResources().getString(R.string.list_user)));
+            AppSettings.savonSwitch(this, AppSettings.USER_VIEW);
+            this.updateAppListView();
+            this.mListTypeName.setText(String.format("(%s)",
+                    this.getResources().getString(R.string.list_user))
+            );
         }
     }
 
-    public void insearch(View view) {
-        EditText edit_insearch = findViewById(R.id.edit_insearch);
-        ImageButton btn_insearch = findViewById(R.id.btn_insearch);
-        initEditaTextAction(edit_insearch);
-        if(edit_insearch.getVisibility() == View.GONE){
-            edit_insearch.setVisibility(View.VISIBLE);
-            btn_insearch.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_close_24, getTheme()));
-            edit_insearch.setOnEditorActionListener((textView, actionId, keyEvent)
-            -> {
-                if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                    ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 0);
-                    return true;
-                }
-                return false;
-            });
-            return;
+    public void onSearchButtonClick(View view) {
+        if(this.mSearchEditText.getVisibility() == View.GONE) {
+            this.mSearchEditText.setVisibility(View.VISIBLE);
+            Drawable drawable = this.getResources().getDrawable(
+                    R.drawable.ic_baseline_close_24, getTheme());
+            this.mSearchButton.setImageDrawable(drawable);
         }
-        edit_insearch.setVisibility(View.GONE);
-        edit_insearch.setText("");
-        initMainActivityListView(getOnSwitchListView(mContext) == USER_VIEW ? userAppList : systemAppList);
-        ((InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(view.getWindowToken(), 0);
-        btn_insearch.setImageDrawable(getResources().getDrawable(R.drawable.ic_baseline_search_24, getTheme()));
-    }
-
-    private void initEditaTextAction(EditText edit_insearch) {
-        if (EditSearchInit) return;
-        edit_insearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-            }
-            @Override
-            public void afterTextChanged(Editable editable) {
-                String inText = editable.toString();
-                initMainActivityListView(searchAppView(inText));
-            }
-        });
-        EditSearchInit = true;
-    }
-
-    private List<PackageInfo> searchAppView(String intext) {
-        List<PackageInfo> appList = getOnSwitchListView(mContext) == USER_VIEW ? userAppList : systemAppList;
-        if (intext.length() < 1) return appList;
-        List<PackageInfo> result = new ArrayList<>();
-        for (PackageInfo info : appList)
-            if (info.packageName.contains(intext) || getPackageManager().getApplicationLabel(info.applicationInfo).toString().contains(intext))
-                result.add(info);
-        return result;
+        else {
+            this.mSearchEditText.setVisibility(View.GONE);
+            Drawable drawable = this.getResources().getDrawable(
+                    R.drawable.ic_baseline_search_24, getTheme());
+            this.mSearchButton.setImageDrawable(drawable);
+            this.mSearchEditText.setText("");
+            this.updateAppListView();
+            this.onEditorAction(null, EditorInfo.IME_ACTION_NEXT, null);
+        }
     }
 
     public void openSettingsActivity(View view) {
         Intent intent = new Intent();
-        intent.setClass(mContext, SettingsActivity.class);
+        intent.setClass(this, SettingsActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        mContext.startActivity(intent);
+        this.startActivity(intent);
+    }
+
+    @Override
+    public void onRefresh() {
+        this.mAppInfoManager.update();
+        this.updateAppListView();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        this.updateAppListView(s.toString());
+    }
+
+    @Override
+    public boolean onEditorAction(TextView textView, int actionId, KeyEvent event) {
+        if(actionId == EditorInfo.IME_ACTION_NEXT) {
+            var systemService = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+            systemService.hideSoftInputFromWindow(this.mSearchButton.getWindowToken(), 0);
+            return true;
+        }
+        return false;
+    }
+
+    public void onBoolbarClick(View view) {
+        this.updateAppListView();
     }
 }
