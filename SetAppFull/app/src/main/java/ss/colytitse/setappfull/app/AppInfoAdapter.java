0187ff84bb20c69dc6a420da2e627f9f369fc0ba
cokkeijigen <itsukezigen@gmail.com>
2025-05-27 00:12:53 +0800

    Update AppInfoAdapter.java

--- SetAppFull/app/src/main/java/ss/colytitse/setappfull/app/AppInfoAdapter.java
+++ SetAppFull/app/src/main/java/ss/colytitse/setappfull/app/AppInfoAdapter.java
@@ -1,11 +1,5 @@
 package ss.colytitse.setappfull.app;
 
-import static ss.colytitse.setappfull.app.AppSettings.MODE_1;
-import static ss.colytitse.setappfull.app.AppSettings.MODE_2;
-import static ss.colytitse.setappfull.app.AppSettings.NO_SET;
-import static ss.colytitse.setappfull.app.AppSettings.getScopeMode;
-import static ss.colytitse.setappfull.app.AppSettings.getSetMode;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.pm.PackageInfo;
@@ -24,39 +18,47 @@ import java.util.List;
 
 import ss.colytitse.setappfull.R;
 
+@SuppressWarnings({"unused"})
+@SuppressLint({"UseSwitchCompatOrMaterialCode", "SetTextI18n", "UseCompatLoadingForDrawables"})
 public class AppInfoAdapter extends BaseAdapter {
 
     private final static String TAG = "test_";
 
     private final List<PackageInfo> packageInfo;
     private final Context context;
+
     public AppInfoAdapter(List<PackageInfo> packageInfo, Context context) {
         this.context = context;
-        this.packageInfo = sortOnSwitchList(packageInfo);
+        this.packageInfo = this.sortOnSwitchList(packageInfo);
     }
 
-    private List<PackageInfo> sortOnSwitchList(List<PackageInfo> appList){
-        List<PackageInfo> onSwitch = new ArrayList<>();
+    private List<PackageInfo> sortOnSwitchList(List<PackageInfo> appList) {
+        List<PackageInfo> result = new ArrayList<>();
         List<PackageInfo> offSwitch = new ArrayList<>();
-        List<PackageInfo> allSwitchStatus = new ArrayList<>();
-        for (PackageInfo info: appList){
-            if(getSetMode(context, info.packageName) != NO_SET)
-                onSwitch.add(info);
-            else offSwitch.add(info);
-        }
-        allSwitchStatus.addAll(onSwitch);
-        allSwitchStatus.addAll(offSwitch);
-        return allSwitchStatus;
+        appList.forEach(info -> {
+            if(AppSettings.getSetMode(context, info.packageName) != AppSettings.NO_SET)
+            {
+                result.add(info);
+            }
+            else {
+                offSwitch.add(info);
+            }
+        });
+        result.addAll(offSwitch);
+        return result;
     }
 
     @Override
     public int getCount() {
-        return packageInfo.size();
+        return this.packageInfo.size();
     }
 
     @Override
-    public Object getItem(int i) {
-        return null;
+    public PackageInfo getItem(int i) {
+        if(i >= this.packageInfo.size()) {
+            return null;
+        }
+        return this.packageInfo.get(i);
     }
 
     @Override
@@ -64,43 +66,48 @@ public class AppInfoAdapter extends BaseAdapter {
         return i;
     }
 
-    @SuppressLint({"UseSwitchCompatOrMaterialCode", "SetTextI18n", "UseCompatLoadingForDrawables"})
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        if (view == null)
-            view = LayoutInflater.from(context).inflate(R.layout.list_item, viewGroup, false);
-        TextView app_name = view.findViewById(R.id.app_name);
-        TextView app_pkgn = view.findViewById(R.id.app_pkgn);
-        TextView app_version = view.findViewById(R.id.app_version);
-        ImageView app_icon = view.findViewById(R.id.app_icon);
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(
+                    R.layout.list_item, viewGroup, false
+            );
+        }
+        TextView appName = view.findViewById(R.id.app_name);
+        TextView appPkgn = view.findViewById(R.id.app_pkgn);
+        TextView appVersion = view.findViewById(R.id.app_version);
+        ImageView appIcon = view.findViewById(R.id.app_icon);
         Switch onSwitch = view.findViewById(R.id.set_switch);
         AppInfo appInfo = new AppInfo(packageInfo.get(i), context);
-        app_version.setText(appInfo.getVersionName() + " (" + appInfo.getVersionCode() + ")");
-        app_icon.setImageDrawable(appInfo.getAppIcon());
-        app_pkgn.setText(appInfo.getPackageName());
-        app_name.setText(appInfo.getAppName());
-        int mode = getSetMode(context, appInfo.getPackageName());
+        appVersion.setText(appInfo.getVersionName() + " (" + appInfo.getVersionCode() + ")");
+        appIcon.setImageDrawable(appInfo.getAppIcon());
+        appPkgn.setText(appInfo.getPackageName());
+        appName.setText(appInfo.getAppName());
+        int mode = AppSettings.getSetMode(context, appInfo.getPackageName());
         LinearLayout item_bac = view.findViewById(R.id.item_root_view);
-        TextView mode_text = view.findViewById(R.id.mode_state);
-        onSwitch.setChecked(mode != NO_SET);
-        if (mode == NO_SET) {
-            mode_text.setText("");
-            item_bac.setBackground(context.getResources().getDrawable(R.drawable.button_background, context.getTheme()));
+        TextView modeText = view.findViewById(R.id.mode_state);
+        onSwitch.setChecked(mode != AppSettings.NO_SET);
+        if (mode ==AppSettings.NO_SET) {
+            modeText.setText("");
+            item_bac.setBackground(context.getResources()
+                    .getDrawable(R.drawable.button_background, context.getTheme()));
         }
-        if (mode == MODE_1){
-            item_bac.setBackground(context.getResources().getDrawable(R.drawable.button_background2, context.getTheme()));
-            mode_text.setText(R.string.mode_1);
+        if (mode == AppSettings.MODE_1) {
+            item_bac.setBackground(context.getResources()
+                    .getDrawable(R.drawable.button_background2, context.getTheme()));
+            modeText.setText(R.string.mode_1);
         }
-        if (mode == MODE_2){
-            item_bac.setBackground(context.getResources().getDrawable(R.drawable.button_background3, context.getTheme()));
-            mode_text.setText(R.string.mode_2);
+        if (mode == AppSettings.MODE_2) {
+            item_bac.setBackground(context.getResources()
+                    .getDrawable(R.drawable.button_background3, context.getTheme()));
+            modeText.setText(R.string.mode_2);
         }
-        mode_text.setVisibility(getScopeMode(context) ? View.GONE : View.VISIBLE);
+        modeText.setVisibility(AppSettings.getScopeMode(context) ? View.GONE : View.VISIBLE);
         onSwitch.setClickable(false);
         return view;
     }
 
-    static public class AppInfo{
+    static public class AppInfo {
         private final PackageInfo packageInfo;
         private final Context context;
 
@@ -114,7 +121,10 @@ public class AppInfoAdapter extends BaseAdapter {
         }
 
         public String getAppName(){
-            return packageInfo.applicationInfo.loadLabel(context.getPackageManager()).toString();
+            if(packageInfo.applicationInfo != null) {
+                return packageInfo.applicationInfo.loadLabel(context.getPackageManager()).toString();
+            }
+            return null;
         }
 
         public String getVersionName(){
@@ -125,8 +135,11 @@ public class AppInfoAdapter extends BaseAdapter {
             return packageInfo.versionCode;
         }
 
-        public Drawable getAppIcon(){
-            return packageInfo.applicationInfo.loadIcon(context.getPackageManager());
+        public Drawable getAppIcon() {
+            if(packageInfo.applicationInfo != null) {
+                return packageInfo.applicationInfo.loadIcon(context.getPackageManager());
+            }
+            return null;
         }
 
 
